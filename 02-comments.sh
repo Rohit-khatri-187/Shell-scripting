7e83570687d018c22702e118faf9694f61973320
Rohit-khatri-187 <khatrirohit1000@gmail.com>
2026-02-04 16:26:47 +0545

    basic shell scripts

---
+++ 02-comments.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Checking comments"
+
+#this is a single line comments
---
+++ 03-vardemo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#script to show how to use variable
+
+a=10
+name="Rohit khatri"
+age=21
+echo "My name is $name and age is $age"
