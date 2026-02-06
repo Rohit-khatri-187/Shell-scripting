c8179d96515e1cd4c5fc0feb39e3c154d027b4c0
Rohit-khatri-187 <khatrirohit1000@gmail.com>
2026-02-06 11:34:05 +0545

    Learned about the loops function and args

---
+++ 19-function-demo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function welcomeNote {
+	echo "---------------"
+	echo "Welcome"
+	echo "---------------"
+}
+
+function Hello {
+	echo "--------------"
+	echo "Hi my name is $1"
+	echo "My age is $2"
+}
+
+
+Hello Rohit 20
+
+
+
+
---
+++ 21-shift-args.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#to create a user , provide username and description
+
+echo "Creating user"
+echo "Username is $1"
+
+shift
+echo "After shifting one argument is $@"
+
+
+
+
+
+
+
+
